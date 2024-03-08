bca7a026e0748dd367a527b2de94b5c91b86d673
tutumi <21241215@kyoto-wu.ac.jp>
2024-03-08 11:51:36 +0900

    202403081151

---
+++ script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+let recommend = document.querySelector(".recommend");
+let artist = document.querySelector(".name");
+let img = document.querySelector("img");
+let font = document.querySelectorAll(".fonts");
+let comment = document.querySelectorAll("blockquote");
+let imp = document.querySelectorAll("span");
+
